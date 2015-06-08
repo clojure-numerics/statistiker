91169fe2fe471fb31ebdddc4b79b799b78b37be5
Mike <mike@mikera.net>
2015-06-08 15:26:18 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure              "1.6.0"]
                  [org.clojure/math.combinatorics   "0.1.1"]
-                 [org.apache.commons/commons-math3 "3.4.1"]
-                 [net.mikera/core.matrix           "0.33.2"]
-                 [prismatic/schema                 "0.2.2"]]
+                 [org.apache.commons/commons-math3 "3.5"]
+                 [net.mikera/core.matrix           "0.35.0"]
+                 [prismatic/schema                 "0.3.0"]]
   :core.typed {:check [clojurewerkz.statistiker.optimization]}
   :source-paths       ["src/clj"]
   :java-source-paths  ["src/java"]
